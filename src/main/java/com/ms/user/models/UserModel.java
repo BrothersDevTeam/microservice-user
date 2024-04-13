d1d9a4344ffdd88dfc881fde3323e64ced8f7a6b
Cristiano Lopes Duarte <tkdcris@gmail.com>
2024-04-13 12:27:22 -0300

    Post user

---
+++ src/main/java/com/ms/user/models/UserModel.java
@@ -0,0 +1,29 @@
+package com.ms.user.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Entity
+@Table(name = "TB_USERS")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserModel implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID userId;
+    private String name;
+    private String email;
+
+
+
+}
