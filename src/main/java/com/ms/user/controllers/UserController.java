3799f24ef9c5c3a0ead76eb9b28bb8d7fbfd8688
Cristiano Lopes Duarte <tkdcris@gmail.com>
2024-04-15 00:01:58 -0300

    CORS config

--- src/main/java/com/ms/user/controllers/UserController.java
+++ src/main/java/com/ms/user/controllers/UserController.java
@@ -8,11 +8,13 @@ import jakarta.validation.Valid;
 import org.springframework.beans.BeanUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class UserController {
 
     final UserService userService;
